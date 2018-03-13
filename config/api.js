c750f24e7206708c9d0dd6a3c8e7768a9a7a7d8f
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-03-13 20:47:08 +0000

    Add config file

---
+++ config/api.js
@@ -0,0 +1,7 @@
+export const api = {
+  url: 'https://api.foursquare.com/v2/venues/explore',
+  client_id: '4YDNGXUYN133HZ4TY4W1JYF1EZDOWWW3E1MLNJ5QAIGSA3VG',
+  client_secret: 'YDOID3BQN1PSE22Y3JHYMCTRZQUGKTPJUWQ0V5X1QNFOEWM0',
+  versioning: 20180313,
+  limit: 5
+}
